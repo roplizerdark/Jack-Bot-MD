b1008dede8cffdaefba1898c43f54f76ebc1576b
maroupp02 <3mkjackyakosomk@gmail.com>
2024-07-08 14:59:41 +0300

    Update بوت.js

--- plugins/بوت.js
+++ plugins/بوت.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
   let name = conn.getName(who);
   let mentionedUsername = `@${who.replace(/@.+/, '')}`; // اسم المستخدم المنشن
 
-  let imgurl = 'https://telegra.ph/file/c761025b7d8387e2daa1d.jpg'; // رابط الصورة
+  let imgurl = 'https://telegra.ph/file/efb1c82a1c11caa338b8e.jpg'; // رابط الصورة
   let filename = 'imgerror.jpg'; // اسم الملف
 
   // رسالة مع التعديل المطلوب
