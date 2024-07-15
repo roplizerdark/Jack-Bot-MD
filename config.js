ffc0f0ed89ed2c1daf2b20f6ff520fa6e2799f58
maroupp02 <3mkjackyakosomk@gmail.com>
2024-07-15 16:07:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
 global.owner = [['201208386317', '🌩️ ẉa.me/MVRO‖.peị!‽ 👑', true], 
- ['201208386317', '🌩️ ẉa.me/MVRO‖.peị!‽ 👑', true],  ['201141045951'], ['201141045951'], ['201141045951'], ['201141045951'], ['201141045951'], ['201066909808'], ['201141045951'], ['201141045951'], ['48459438480']]
+ ['201208386317', '🌩️ ẉa.me/MVRO‖.peị!‽ 👑', true],  ['201141045951'], ['201141045951'], ['201141045951'], ['201141045951'], ['201141045951'], ['201066909808'], ['201141045951'], ['201148980831'], ['201066909808']]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
