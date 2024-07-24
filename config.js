81fa2e5a38e53e6545f45211b2d6fe09ee8fb255
dakhkfgkg <roplizera1@gmail.com>
2024-07-25 00:45:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,24 +3,24 @@ import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
 global.owner = [['201274605652', 'Adham khaled', true], 
- ['201080898312', '𝑀𝑉𝑅𝛩 𝐵𝛩𝑇', true],  ['201141045951'], ['201141045951'], ['201141045951'], ['201141045951'], ['201141045951'], ['201066909808'], ['201159596672'], ['201148980831'], ['201066909808']]
+ ['201080898312', 'Adham 𝐵𝛩𝑇', true],  ['201141045951'], ['201141045951'], ['201141045951'], ['201141045951'], ['201141045951'], ['201066909808'], ['201159596672'], ['201148980831'], ['201066909808']]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
 global.botNumberCode = '' //Ejemplo: +201208386317
 global.confirmCode = ''
 
-global.animxscans = ['201208386317']
-global.suittag = ['201208386317']
+global.animxscans = ['201274605652']
+global.suittag = ['201274605652']
 global.mods = []
 global.prems = []
 
-global.packname = '𝟎𝟏𝟐𝟎𝟖𝟑𝟖𝟔𝟑𝟏𝟕'
-global.author = '𝑀𝑉𝑅𝛩 𝐵𝛩𝑇'
-global.wm = '𝑀𝑉𝑅𝛩 𝐵𝛩𝑇'
-global.wm2 = '𝑀𝑉𝑅𝛩 𝐵𝛩𝑇'
-global.azami = '𝑀𝑉𝑅𝛩 𝐵𝛩𝑇'
-global.cb = '𝑀𝑉𝑅𝛩 𝐵𝛩𝑇'
+global.packname = '201274605652'
+global.author = 'Astro 𝐵𝛩𝑇'
+global.wm = 'Astro 𝐵𝛩𝑇'
+global.wm2 = 'Astro 𝐵𝛩𝑇'
+global.azami = 'Astro 𝐵𝛩𝑇'
+global.cb = 'Astro 𝐵𝛩𝑇'
 
 global.vs = 'V2 • 1.0.5'
 global.library = 'Baileys'
@@ -28,8 +28,8 @@ global.baileys = '@whiskeysockets/baileys'
 global.lenguaje = 'Español'
 global.KMA = '╰━━━〔 *🛡️ 1.7.9* 〕━━━━━⬣'
 global.menudi = ['⛶','❏','⫹⫺']
-global.dev = '𝑀𝑉𝑅𝛩 𝐵𝛩𝑇'
-global.devnum = '201208386317'
+global.dev = 'Astro 𝐵𝛩𝑇'
+global.devnum = '201274605652'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => { unwatchFile(file)
