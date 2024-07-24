5267c8c0b2bf89ff120b4d70633bac366407af5e
dakhkfgkg <roplizera1@gmail.com>
2024-07-25 00:47:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
 global.owner = [['201274605652', 'Adham khaled', true], 
- ['201080898312', 'Adham 𝐵𝛩𝑇', true],  ['201141045951'], ['201141045951'], ['201141045951'], ['201141045951'], ['201141045951'], ['201066909808'], ['201159596672'], ['201148980831'], ['201066909808']]
+ ['201080898312', 'Adham 𝐵𝛩𝑇', false],  ['201141045951'], ['201141045951'], ['201141045951'], ['201141045951'], ['201141045951'], ['201066909808'], ['201159596672'], ['201148980831'], ['201066909808']]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
