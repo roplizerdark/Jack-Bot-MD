cee28cd83b639b02573706835bb3b0ddd4014f6a
dakhkfgkg <roplizera1@gmail.com>
2024-07-25 17:49:24 +0200

    Update info-reporte.js

--- plugins/info-reporte.js
+++ plugins/info-reporte.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (text.length < 10) throw `*[❗معلومه❗] ل اترسل اقل من 10 احرف!*`;
   if (text.length > 1000) throw `*[❗معلومه❗] لا ترسل اكثر من 1000 حرف!*`;
   const teks = `*❒═════[ابلاغ]═════❒*\n*┬*\n*├❧ الرقم:* wa.me/${m.sender.split`@`[0]}\n*┴*\n*┬*\n*├❧ الرساله:* ${text}\n*┴*`;
-  conn.reply('201012531172@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+  conn.reply('201274605652@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
   m.reply(`*[ ✔️ ]  تم بنجاح، تم إرسال التقرير إلى مطوري.  سيكون لديك الجواب قريبا.  إذا تم اكتشاف أن التقرير خاطئ، فسيتم تجاهل التقرير.*`);
 };
 handler.help = ['reporte', 'request'].map((v) => v + ' <teks>');
