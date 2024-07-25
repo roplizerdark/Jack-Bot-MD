5563c5f1cba5075d8ad36f4c2774c45bb46c7517
dakhkfgkg <roplizera1@gmail.com>
2024-07-25 19:03:56 +0200

    Update owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
-await conn.reply(id, '*وداعا احبكم سأخرج الان من الجروب! (≧ω≦)ゞ*') 
+await conn.reply(id, '* وداعا احبكم سأخرج الان من الجروب مطوري طلب مني الخروج سلام! (≧ω≦)ゞ*') 
 await conn.groupLeave(id)}
 handler.tags = ['owner']
 handler.command = /^(اخرج|leavegc|برا|salirdelgrupo)$/i
