2099a127dd742d1a8f7f90b477c9d23cecd365af
roplizerop <roplizera2@gmail.com>
2024-07-26 14:50:36 +0200

    Update بايدن.js

--- plugins/بايدن.js
+++ plugins/بايدن.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-  let too = `[❗] افصل النص يحب بـ *+*\n\n *مـثــال* :\n*${usedPrefix + command}* مارو معانا+مارو ضايع`
+  let too = `[❗] افصل النص يحب بـ *+*\n\n *مـثــال* :\n*${usedPrefix + command}* استرو معانا+استرو ضايع`
 
   if (!text) throw too
   let lr = (`https://api.popcat.xyz/biden?text=${encodeURIComponent(text)}`)
   conn.sendFile(m.chat, lr, 'drake.png', `تم بواسطه ✅
-  𝙼𝚅𝚁𝙾⚡3𝑀𝐾`, m)
+  adham⚡3𝑀𝐾`, m)
 }
 handler.help = ['drake']
 handler.tags = ['maker']
