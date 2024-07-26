2a22d26487007be97ffecc85b63947cf4e93f3b5
roplizerop <roplizera2@gmail.com>
2024-07-26 15:13:10 +0200

    Update رابطي.js

--- plugins/رابطي.js
+++ plugins/رابطي.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text }) => {
-  let tagme = `https://wa.me/+${m.sender.replace(`+`)}/?text=BY+『🔥┇𝚖𝚟𝚛𝚘 𝚋𝚘𝚝』`
+  let tagme = `https://wa.me/+${m.sender.replace(`+`)}/?text=BY+『🔥┇astro 𝚋𝚘𝚝』`
   let mylink = [m.sender]
   conn.reply(m.chat, tagme, m, { contextInfo: { mylink }})
 }
